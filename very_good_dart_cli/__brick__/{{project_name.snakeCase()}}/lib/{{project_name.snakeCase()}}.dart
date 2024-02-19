10dbdffdc2ada61c809f52af5fe3a857b3f5f8df
Alejandro Santiago <dev@alestiago.com>
2024-02-19 11:29:40 +0000

    chore: migrate very_good_dart_cli

---
+++ very_good_dart_cli/__brick__/{{project_name.snakeCase()}}/lib/{{project_name.snakeCase()}}.dart
@@ -0,0 +1,10 @@
+/// {{project_name.snakeCase()}}, {{description}}
+///
+/// ```sh
+/// # activate {{project_name.snakeCase()}}
+/// dart pub global activate {{project_name.snakeCase()}}
+///
+/// # see usage
+/// {{executable_name.snakeCase()}} --help
+/// ```
+library {{project_name.snakeCase()}};
---
+++ very_good_dart_cli/hooks/post_gen.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+import 'package:meta/meta.dart';
+
+/// Type definition for [Process.run].
+typedef RunProcess = Future<ProcessResult> Function(
+  String executable,
+  List<String> arguments, {
+  String? workingDirectory,
+  bool runInShell,
+});
+
+Future<void> run(HookContext context,
+// We intentionally ignore the trailing comma until the following mason issue is
+// fixed: https://github.com/felangel/mason/pull/1164
+// ignore: require_trailing_commas
+    {@visibleForTesting RunProcess runProcess = Process.run}) async {
+  final projectName = context.vars['project_name'] as String;
+
+  final progress = context.logger.progress('Getting Dart dependencies...');
+
+  // We have to `pub get` the generated project to ensure that the analysis
+  // is able to fix the imports with the correct analysis options.
+  await runProcess(
+    'dart',
+    [
+      'pub',
+      'get',
+      '--directory=$projectName',
+    ],
+    workingDirectory: Directory.current.path,
+  );
+
+  progress.update('Fixing Dart imports ordering...');
+
+  // Some imports are relative to the user specified package name, hence
+  // we try to fix the import directive ordering after the template has
+  // been generated.
+  //
+  // We only fix for the [directives_ordering](https://dart.dev/tools/linter-rules/directives_ordering)
+  // linter rules, as the other rule should be tackled by the template itself.
+  await runProcess(
+    'dart',
+    [
+      'fix',
+      projectName,
+      '--apply',
+      '--code=directives_ordering',
+    ],
+    workingDirectory: Directory.current.path,
+  );
+
+  progress.complete('Completed post generation');
+}
