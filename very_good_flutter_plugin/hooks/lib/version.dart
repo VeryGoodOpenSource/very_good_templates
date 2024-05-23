4d227ad31e86484b4a63c45b5c5d440f38b5ea43
Alejandro Santiago <dev@alestiago.com>
2024-05-23 14:30:55 +0100

    chore(very_good_flutter_plugin): ensure template uses Flutter 3.22 with Dart 3.4 (#105)

--- very_good_flutter_plugin/hooks/lib/version.dart
+++ very_good_flutter_plugin/hooks/lib/version.dart
@@ -8,7 +8,7 @@ library;
 const $templateVersion = '0.7.0';
 
 /// The Flutter version associated with the template version.
-const $flutterVersion = '3.19.0';
+const $flutterVersion = '3.22.0';
 
 /// The minimum Dart version required by the template.
 ///
@@ -18,4 +18,4 @@ const $flutterVersion = '3.19.0';
 /// See also:
 ///
 /// * [Flutter SDK archive](https://docs.flutter.dev/release/archive)
-const $minDartVersion = '3.3.0';
+const $minDartVersion = '3.4.0';
