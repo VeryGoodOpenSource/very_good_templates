46a138122f4d5d4a8668ab32770b55ab9fb0cfd3
Alejandro Santiago <dev@alestiago.com>
2024-07-01 17:34:02 +0100

    test: update parseDirectHostedDependencies test

--- tool/dependency_tightener/test/src/pubspec_parse_test.dart
+++ tool/dependency_tightener/test/src/pubspec_parse_test.dart
@@ -9,14 +9,20 @@ void main() {
   test('parses templated pubspec', () async {
     final dependencies = parseDirectHostedDependencies(pubspec1);
 
-    expect(dependencies, hasLength(2));
-
     expect(
       dependencies,
       containsAll({
         HostedDependency(
           version: VersionConstraint.parse('^2.1.0'),
           hosted: HostedDetails('plugin_platform_interface', null),
+        ),
+        HostedDependency(
+          version: VersionConstraint.parse('0.3.0'),
+          hosted: HostedDetails('pub_updater', null),
+        ),
+        HostedDependency(
+          version: VersionConstraint.parse('^1.0.3'),
+          hosted: HostedDetails('mocktail', null),
         ),
         HostedDependency(
           version: VersionConstraint.parse('^5.1.0'),
