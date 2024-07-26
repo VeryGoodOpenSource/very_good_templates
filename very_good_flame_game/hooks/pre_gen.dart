c3214aeb60f585ee6d40079f619f835e6a8a43be
Alejandro Santiago <dev@alestiago.com>
2024-07-26 12:15:57 +0100

    feat(very_good_flame_game): allow use of application_id var (#157)

---
+++ very_good_flame_game/hooks/pre_gen.dart
@@ -0,0 +1,46 @@
+import 'package:mason/mason.dart';
+import 'package:very_good_flame_game_hooks/very_good_core_hooks.dart';
+
+void run(HookContext context) {
+  final configuration =
+      VeryGoodFlameGameConfiguration.fromHookVars(context.vars);
+
+  context.vars = {
+    /// Below are all the variables that are accessible in the templates.
+    ///
+    /// You can access them using the Mustache syntax within the template files
+    /// (those under `__brick__`). For example:
+    ///
+    /// ```dart
+    /// // __brick__/pubspec.yaml
+    /// name: {{project_name}}
+    /// ```
+    ///
+    /// This will be replaced with the value of the `project_name` variable.
+    ///
+    /// By design, Mustache is a  "logic-less" template language, meaning it
+    /// does not have the ability to construct logical expressions. From the
+    /// documentation:
+    ///
+    /// > We call it "logic-less" because there are no if statements, else
+    /// > clauses, or for loops. Instead there are only tags.
+    ///
+    /// This means that you can't use Mustache to add logical expressions to
+    /// your templates. If you need to add logical expressions, you should
+    /// evaluate the expression within this pre-generation hook (`pre_gen.dart`)
+    /// and pass the result as a variable to the template.
+    ///
+    /// See also:
+    ///
+    /// * [Mustache documentation](https://mustache.github.io/mustache.5.html)
+    /// * [Mason conditional documentation](https://docs.brickhub.dev/brick-syntax#-conditionals)
+    'project_name': configuration.projectName,
+    'org_name': configuration.organizationName,
+    'description': configuration.description,
+    'android_namespace': configuration.androidNamespace,
+    'android_application_id': configuration.androidApplicationId,
+    'ios_application_id': configuration.iOsApplicationId,
+    'macos_application_id': configuration.macOsApplicationId,
+    'windows_application_id': configuration.windowsApplicationId,
+  };
+}
---
+++ very_good_flame_game/hooks/test/pre_gen_test.dart
@@ -0,0 +1,48 @@
+import 'package:mason/mason.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../pre_gen.dart' as pre_gen;
+
+class _MockHookContext extends Mock implements HookContext {}
+
+void main() {
+  group('pre_gen', () {
+    late HookContext context;
+
+    setUp(() {
+      context = _MockHookContext();
+    });
+
+    test('populates variables', () {
+      final vars = {
+        'project_name': 'my_game',
+        'org_name': 'com.example',
+        'application_id': 'app.id',
+        'description': 'A new Flame project.',
+      };
+      when(() => context.vars).thenReturn(vars);
+
+      pre_gen.run(context);
+
+      final newVars = verify(() => context.vars = captureAny()).captured.last
+          as Map<String, dynamic>;
+
+      expect(
+        newVars,
+        equals(
+          {
+            'project_name': 'my_game',
+            'org_name': 'com.example',
+            'description': 'A new Flame project.',
+            'android_namespace': 'app.id',
+            'android_application_id': 'app.id',
+            'ios_application_id': 'app.id',
+            'macos_application_id': 'app.id',
+            'windows_application_id': 'app.id',
+          },
+        ),
+      );
+    });
+  });
+}
