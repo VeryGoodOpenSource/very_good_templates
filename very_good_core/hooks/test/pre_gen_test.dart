43cefacdc1b73d2f1c3fba6bb128459ea121e4e8
Antoniobox <antonioboxsanchez@gmail.com>
2024-08-27 19:22:59 +0200

    chore: Update clock usage to use withClock

--- very_good_core/hooks/test/pre_gen_test.dart
+++ very_good_core/hooks/test/pre_gen_test.dart
@@ -17,38 +17,39 @@ void main() {
 
     test('populates variables', () {
       // Fake the current year to ensure the test is deterministic.
-      final clock = Clock.fixed(DateTime(2024));
+      final clock = Clock.fixed(DateTime.now());
+      withClock(clock, () {
+        final vars = {
+          'project_name': 'my_app',
+          'org_name': 'com.example',
+          'application_id': 'app.id',
+          'description': 'A new Flutter project.',
+          'current_year': clock.now().year,
+        };
+        when(() => context.vars).thenReturn(vars);
 
-      final vars = {
-        'project_name': 'my_app',
-        'org_name': 'com.example',
-        'application_id': 'app.id',
-        'description': 'A new Flutter project.',
-        'current_year': clock.now().year,
-      };
-      when(() => context.vars).thenReturn(vars);
+        pre_gen.run(context);
 
-      pre_gen.run(context);
+        final newVars = verify(() => context.vars = captureAny()).captured.last
+            as Map<String, dynamic>;
 
-      final newVars = verify(() => context.vars = captureAny()).captured.last
-          as Map<String, dynamic>;
-
-      expect(
-        newVars,
-        equals(
-          {
-            'project_name': 'my_app',
-            'org_name': 'com.example',
-            'description': 'A new Flutter project.',
-            'android_namespace': 'app.id',
-            'android_application_id': 'app.id',
-            'ios_application_id': 'app.id',
-            'macos_application_id': 'app.id',
-            'windows_application_id': 'app.id',
-            'current_year': clock.now().year,
-          },
-        ),
-      );
+        expect(
+          newVars,
+          equals(
+            {
+              'project_name': 'my_app',
+              'org_name': 'com.example',
+              'description': 'A new Flutter project.',
+              'android_namespace': 'app.id',
+              'android_application_id': 'app.id',
+              'ios_application_id': 'app.id',
+              'macos_application_id': 'app.id',
+              'windows_application_id': 'app.id',
+              'current_year': clock.now().year,
+            },
+          ),
+        );
+      });
     });
   });
 }
