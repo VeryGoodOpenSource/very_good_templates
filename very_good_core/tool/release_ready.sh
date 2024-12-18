06d28e5fd9dd9de36757d1ebd7f58243c78717bf
Tom Arra <tarra3@gmail.com>
2024-12-18 08:20:16 -0600

    chore(very_good_core): v0.10.0 (#225)

--- very_good_core/tool/release_ready.sh
+++ very_good_core/tool/release_ready.sh
@@ -68,7 +68,7 @@ fi
 echo "# ${new_version} \n\n${commits}\n\n$(cat CHANGELOG.md)" > CHANGELOG.md
 echo "CHANGELOG generated, validate entries here: $(pwd)/CHANGELOG.md"
 
-echo "Creating git branch for ver_good_cli@$new_version"
+echo "Creating git branch for very_good_core@$new_version"
 git checkout -b "chore/very_good_core-v$new_version" > /dev/null
 
 git add brick.yaml CHANGELOG.md 
