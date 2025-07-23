ba143671c9df64b3dd909b476bf5a9fd01f4d975
matiasleyba <matileyba@gmail.com>
2025-07-23 14:17:59 -0300

    fix: formatting

--- very_good_dart_package/__brick__/test/src/{{project_name.snakeCase()}}_test.dart
+++ very_good_dart_package/__brick__/test/src/{{project_name.snakeCase()}}_test.dart
@@ -6,11 +6,7 @@ import 'package:{{project_name.snakeCase()}}/{{project_name.snakeCase()}}.dart';
 void main() {
   group('{{project_name.pascalCase()}}', () {
     test('can be instantiated', () {
-      expect(
-          {
-            {project_name.pascalCase()}
-          }(),
-          isNotNull);
+      expect({{project_name.pascalCase()}}(), isNotNull);
     });
   });
 }
