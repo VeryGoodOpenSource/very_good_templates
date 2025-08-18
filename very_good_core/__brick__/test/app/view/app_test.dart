6970fa55feaf2912c01b64fea560933784fdc396
Erick <erickzanardoo@gmail.com>
2025-08-18 10:07:23 -0300

    fix: Guarantee 100% coverage on core app (#334)

--- very_good_core/__brick__/test/app/view/app_test.dart
+++ very_good_core/__brick__/test/app/view/app_test.dart
@@ -1,3 +1,6 @@
+// Ignore for testing purposes
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:{{project_name.snakeCase()}}/app/app.dart';
 import 'package:{{project_name.snakeCase()}}/counter/counter.dart';
@@ -5,7 +8,7 @@ import 'package:{{project_name.snakeCase()}}/counter/counter.dart';
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
+      await tester.pumpWidget(App());
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
