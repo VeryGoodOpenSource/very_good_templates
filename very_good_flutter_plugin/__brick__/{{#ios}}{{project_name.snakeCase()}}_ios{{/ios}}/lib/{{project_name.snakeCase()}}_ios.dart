eb90aa5f0f4cc4a4ec0c0d40fc8598d6ec3898b7
Marcos Sevilla <marcossevilla14@gmail.com>
2025-11-05 09:33:32 +0100

    refactor(ios): use host api

--- very_good_flutter_plugin/__brick__/{{#ios}}{{project_name.snakeCase()}}_ios{{/ios}}/lib/{{project_name.snakeCase()}}_ios.dart
+++ very_good_flutter_plugin/__brick__/{{#ios}}{{project_name.snakeCase()}}_ios{{/ios}}/lib/{{project_name.snakeCase()}}_ios.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
+import 'package:{{project_name.snakeCase()}}_ios/src/messages.g.dart';
 import 'package:{{project_name.snakeCase()}}_platform_interface/{{project_name.snakeCase()}}_platform_interface.dart';
 
+/// {@template {{project_name.snakeCase()}}_ios}
 /// The iOS implementation of [{{project_name.pascalCase()}}Platform].
+/// {@endtemplate}
 class {{project_name.pascalCase()}}IOS extends {{project_name.pascalCase()}}Platform {
-  /// The method channel used to interact with the native platform.
-  @visibleForTesting
-  final methodChannel = const MethodChannel('{{project_name.snakeCase()}}_ios');
+  /// {@macro {{project_name.snakeCase()}}_android}
+  {{project_name.pascalCase()}}IOS({
+    @visibleForTesting {{project_name.pascalCase()}}Api? api,
+  }) : api = api ?? {{project_name.pascalCase()}}Api();
+
+  /// The API used to interact with the native platform.
+  final {{project_name.pascalCase()}}Api api;
 
   /// Registers this class as the default instance of [{{project_name.pascalCase()}}Platform]
   static void registerWith() {
@@ -15,6 +21,6 @@ class {{project_name.pascalCase()}}IOS extends {{project_name.pascalCase()}}Plat
 
   @override
   Future<String?> getPlatformName() {
-    return methodChannel.invokeMethod<String>('getPlatformName');
+    return api.getPlatformName();
   }
 }
--- very_good_flutter_plugin/__brick__/{{#ios}}{{project_name.snakeCase()}}_ios{{/ios}}/test/{{project_name.snakeCase()}}_ios_test.dart
+++ very_good_flutter_plugin/__brick__/{{#ios}}{{project_name.snakeCase()}}_ios{{/ios}}/test/{{project_name.snakeCase()}}_ios_test.dart
@@ -1,44 +1,42 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:{{project_name.snakeCase()}}_ios/src/messages.g.dart';
 import 'package:{{project_name.snakeCase()}}_ios/{{project_name.snakeCase()}}_ios.dart';
 import 'package:{{project_name.snakeCase()}}_platform_interface/{{project_name.snakeCase()}}_platform_interface.dart';
+
+class _Mock{{project_name.pascalCase()}}Api extends Mock
+    implements {{project_name.pascalCase()}}Api {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('{{project_name.pascalCase()}}IOS', () {
+  group({{project_name.pascalCase()}}IOS, () {
     const kPlatformName = 'iOS';
     late {{project_name.pascalCase()}}IOS {{project_name.camelCase()}};
-    late List<MethodCall> log;
+    late {{project_name.pascalCase()}}Api api;
 
     setUp(() async {
-      {{project_name.camelCase()}} = {{project_name.pascalCase()}}IOS();
-
-      log = <MethodCall>[];
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockMethodCallHandler({{project_name.camelCase()}}.methodChannel, (methodCall) async {
-        log.add(methodCall);
-        switch (methodCall.method) {
-          case 'getPlatformName':
-            return kPlatformName;
-          default:
-            return null;
-        }
-      });
+      api = _Mock{{project_name.pascalCase()}}Api();
+      {{project_name.camelCase()}} = {{project_name.pascalCase()}}IOS(api: api);
     });
 
     test('can be registered', () {
       {{project_name.pascalCase()}}IOS.registerWith();
-      expect({{project_name.pascalCase()}}Platform.instance, isA<{{project_name.pascalCase()}}IOS>());
+      expect(
+        {{project_name.pascalCase()}}Platform.instance,
+        isA<{{project_name.pascalCase()}}IOS>(),
+      );
     });
 
     test('getPlatformName returns correct name', () async {
-      final name = await {{project_name.camelCase()}}.getPlatformName();
-      expect(
-        log,
-        <Matcher>[isMethodCall('getPlatformName', arguments: null)],
+      when(api.getPlatformName).thenAnswer((_) async => kPlatformName);
+
+      await expectLater(
+        {{project_name.camelCase()}}.getPlatformName(),
+        completion(equals(kPlatformName)),
       );
-      expect(name, equals(kPlatformName));
+
+      verify(api.getPlatformName).called(1);
     });
   });
 }
