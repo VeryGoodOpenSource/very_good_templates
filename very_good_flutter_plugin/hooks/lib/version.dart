f0193c7c52b41e2748152d30aa35db71da89222c
Marcos Sevilla <marcossevilla14@gmail.com>
2025-11-18 13:48:01 +0100

    feat: upgrade to flutter 3.38.3

--- very_good_flutter_plugin/hooks/lib/version.dart
+++ very_good_flutter_plugin/hooks/lib/version.dart
@@ -8,7 +8,7 @@ library;
 const $templateVersion = '1.0.0';
 
 /// The Flutter version associated with the template version.
-const $flutterVersion = '3.35.0';
+const $flutterVersion = '3.38.3';
 
 /// The minimum Dart version required by the template.
 ///
@@ -18,4 +18,4 @@ const $flutterVersion = '3.35.0';
 /// See also:
 ///
 /// * [Flutter SDK archive](https://docs.flutter.dev/release/archive)
-const $minDartVersion = '3.9.0';
+const $minDartVersion = '3.10.0';
