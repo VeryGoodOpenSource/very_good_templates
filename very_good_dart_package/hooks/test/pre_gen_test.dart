09a0add4cfd39870a5d8368be2bf75b0eb47f1a8
Marcos Sevilla <me@marcossevilla.dev>
2026-04-10 13:10:17 +0200

    add dart_pkg hooks

---
+++ very_good_dart_package/hooks/test/pre_gen_test.dart
@@ -0,0 +1,47 @@
+import 'package:clock/clock.dart';
+import 'package:mason/mason.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../pre_gen.dart' as pre_gen;
+
+class _MockHookContext extends Mock implements HookContext {}
+
+void main() {
+  group('pre_gen', () {
+    late HookContext context;
+
+    setUp(() {
+      context = _MockHookContext();
+    });
+
+    test('populates variables', () {
+      withClock(Clock.fixed(DateTime(2020)), () {
+        final vars = {
+          'project_name': 'my_package',
+          'org_name': 'Very Good Ventures',
+          'description': 'A very good dart package.',
+          'publishable': false,
+        };
+        when(() => context.vars).thenReturn(vars);
+
+        pre_gen.run(context);
+
+        final newVars =
+            verify(() => context.vars = captureAny()).captured.last
+                as Map<String, dynamic>;
+
+        expect(
+          newVars,
+          equals({
+            'project_name': 'my_package',
+            'org_name': 'Very Good Ventures',
+            'description': 'A very good dart package.',
+            'publishable': false,
+            'current_year': '2020',
+          }),
+        );
+      });
+    });
+  });
+}
