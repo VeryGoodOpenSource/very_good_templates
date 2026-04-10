c08819965881b2f09d7c0e03bfc4843096bfdbb9
Marcos Sevilla <31174242+marcossevilla@users.noreply.github.com>
2026-04-10 14:04:43 +0200

    chore(ci): fix workflows (#502)

--- very_good_flutter_plugin/__brick__/{{#windows}}{{project_name.snakeCase()}}_windows{{/windows}}/lib/{{project_name.snakeCase()}}_windows.dart
+++ very_good_flutter_plugin/__brick__/{{#windows}}{{project_name.snakeCase()}}_windows{{/windows}}/lib/{{project_name.snakeCase()}}_windows.dart
@@ -3,14 +3,19 @@ import 'package:flutter/services.dart';
 import 'package:{{project_name.snakeCase()}}_platform_interface/{{project_name.snakeCase()}}_platform_interface.dart';
 
 /// The Windows implementation of [{{project_name.pascalCase()}}Platform].
-class {{project_name.pascalCase()}}Windows extends {{project_name.pascalCase()}}Platform {
+class {{project_name.pascalCase()}}Windows
+    extends {{project_name.pascalCase()}}Platform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel('{{project_name.snakeCase()}}_windows');
+  final methodChannel = const MethodChannel(
+    '{{project_name.snakeCase()}}_windows',
+  );
 
-  /// Registers this class as the default instance of [{{project_name.pascalCase()}}Platform]
+  /// Registers this class as the default instance of
+  /// [{{project_name.pascalCase()}}Platform].
   static void registerWith() {
-    {{project_name.pascalCase()}}Platform.instance = {{project_name.pascalCase()}}Windows();
+    {{project_name.pascalCase()}}Platform.instance =
+        {{project_name.pascalCase()}}Windows();
   }
 
   @override
