ab691f49535284e5b2245e61f9e1a901df737bad
vgvbot <71841508+vgvbot@users.noreply.github.com>
2026-04-16 13:08:01 +0200

    chore(very_good_flutter_plugin): 1.3.2 (#495)

--- very_good_flutter_plugin/hooks/lib/version.dart
+++ very_good_flutter_plugin/hooks/lib/version.dart
@@ -6,7 +6,7 @@
 library;
 
 /// The version of the template.
-const $templateVersion = '1.3.1'; // x-release-please-version
+const $templateVersion = '1.3.2'; // x-release-please-version
 
 /// The Flutter version associated with the template version.
 const $flutterVersion = '3.41.1';
